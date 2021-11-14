879d34d344bc570f3195cbaf331d3e065fbd4f92
Devdutt Shenoi <devdutt@outlook.in>
2021-11-14 13:13:27 +0530

    Make config deserializable

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,11 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
 pub struct Config {
     pub auth: Option<Auth>,
 }
 
+#[derive(Deserialize, Clone)]
 pub struct Auth {
     pub ca_cert_file: String,
     pub cert_file: String,
